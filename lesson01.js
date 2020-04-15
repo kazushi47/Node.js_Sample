7273c6bcf482ff68615575c57e3ec83a6ac90205
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-15 17:18:54 +0900

    Update lesson01.js
    Repair code.

--- lesson01.js
+++ lesson01.js
@@ -1,9 +1,9 @@
 var http = require('http');
 const PORT = 80;
 
-http.createServer((reqest, response) => {
+http.createServer((request, response) => {
     response.write("Hello Node.js!!!");
     response.end();
 }).listen(PORT);
 
-console.log("Server running at http://kazushi-sugitani.ml:${PORT}");
+console.log('Server running at http://kazushi-sugitani.ml:${PORT}');
