2913010bca5ac4ed1c17112e350a3b4b09153112
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-16 15:44:38 +0900

    Update server.js
    Repair code.

--- lesson08/server.js
+++ lesson08/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
 const qs = require('querystring');
 const settings = require('./settings');
 const server = http.createServer();
