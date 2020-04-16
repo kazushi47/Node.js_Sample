693d2d99d5e058c4db81a6c3bf21c8360ac66e50
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 02:04:52 +0900

    Update mongo.js
    Repair code.

--- lesson10/mongo.js
+++ lesson10/mongo.js
@@ -10,11 +10,13 @@ MongoClient.connect(url, connectOption, (err, client) => {
     if (err) return console.dir(err);
     console.log("Connected to db");
 
-    const stream = collection.find().stream();
-    stream.on("data", (item) => {
-        console.log(item);
-    });
-    stream.on("end", () => {
-        console.log("finished.");
+    client.db(settings.db).collection('users', (err, collection) => {
+        const stream = collection.find().stream();
+        stream.on("data", (item) => {
+            console.log(item);
+        });
+        stream.on("end", () => {
+            console.log("finished.");
+        });
     });
 });
