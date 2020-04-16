5ef06ad524c26b6b971678d3aa844d31ee7c0c91
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-16 15:01:34 +0900

    Update server.js
    Repair code.

--- lesson07/server.js
+++ lesson07/server.js
@@ -7,16 +7,21 @@ const server = http.createServer();
 var n = 0;
 
 server.on('request', (req, res) => {
-    console.log("n++");
-    n++;
-    var data = ejs.render(template, {
-        title: "hello",
-        content: "<strong>World!<strong>",
-        n: n
-    });
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+    if (req.url == '/') {
+        n++;
+        var data = ejs.render(template, {
+            title: "hello",
+            content: "<strong>World!<strong>",
+            n: n
+        });
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('Not Found!');
+        res.end();
+    }
 });
 server.listen(settings.port);
 console.log("Server running...");
