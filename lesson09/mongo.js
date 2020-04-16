fc17686acbaebbfe1512c5f61ed3f6b9366bff51
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 00:39:46 +0900

    Update mongo.js
    Repair code.

--- lesson09/mongo.js
+++ lesson09/mongo.js
@@ -2,17 +2,14 @@ const MongoClient = require('mongodb').MongoClient;
 const settings = require('./settings');
 
 MongoClient.connect("mongodb://localhost/" + settings.db, (err, db) => {
-    if (err) throw err;
     console.log("Connected to db");
     db.collection("users", (err, collection) => {
-        if (err) throw err;
         var docs = [
             {name: "taguchi", score: 40},
             {name: "fkoji", score: 80},
             {name: "dotinstall", score: 60}
         ];
         collection.insert(docs, (err, result) => {
-            if (err) throw err;
             console.dir(result);
         });
     });
