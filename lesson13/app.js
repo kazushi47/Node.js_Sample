20c078ba6d14d99e6d0884df251eeb7edb3fa138
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 17:18:44 +0900

    Add file.
    Use Ejs on Express.

---
+++ lesson13/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(8124);
+console.log('Server running...');
