5b3e24d75f5a9fbf89c56032ad2d7d06dd1ab45f
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 14:50:32 +0900

    Update app.js
    Repair code.

--- lesson12/app.js
+++ lesson12/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+app.get('/about', (req, res) => {
+    res.send('about this page!');
+});
 
 app.listen(8124);
 console.log("Server running...");
