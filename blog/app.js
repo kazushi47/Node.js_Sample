e7662499d8dbe9f1291d86032c44827133772c95
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 18:25:18 +0900

    Update app.js
    Repair code.

--- blog/app.js
+++ blog/app.js
@@ -10,10 +10,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', post.index);
-app.get('/posts/new', post.new);
-app.post('/posts/create', post.create);
-app.get('/posts/:id', post.show);
-app.get('/posts/:id/edit', post.edit);
-app.put('/posts/:id/', post.update);
-app.put('/posts/:id/', post.update);
-app.delete('/posts/:id/', post.destroy);
+//app.get('/posts/new', post.new);
+//app.post('/posts/create', post.create);
+//app.get('/posts/:id', post.show);
+//app.get('/posts/:id/edit', post.edit);
+//app.put('/posts/:id/', post.update);
+//app.put('/posts/:id/', post.update);
+//app.delete('/posts/:id/', post.destroy);
