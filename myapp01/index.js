b2f6c448b075d62b2100450b0fc0a27b82c538ae
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-18 04:11:34 +0900

    Update files
    Repair code.

--- myapp01/index.js
+++ myapp01/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const dateutils = require('date-utils');
 
 app.set('views', __dirname);
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const now = new Date().toFormat('YYYY年MM月DD日 H時MI分SS秒');
+    res.render('index', {now: now});
 });
 
 app.listen(8124);
