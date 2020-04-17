736216f2f8f21f6618023b9ec0173788546b8f56
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 18:16:47 +0900

    Update app.js
    Repair code.

--- blog/app.js
+++ blog/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const post = require('./routes/post');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(methodOverride((req, res) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        const method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 
 app.get('/', post.index);
 app.get('/posts/new', post.new);
