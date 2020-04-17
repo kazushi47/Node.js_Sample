f7a7df62296b713400e93b73789a6f7b3f99d12e
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-17 17:36:30 +0900

    Add code
    Use POST.

--- lesson13/app.js
+++ lesson13/app.js
@@ -4,8 +4,15 @@ const app = express();
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', (req, res) => {
     res.render('index', {title: 'Hello EJS & Express!!!'});
+});
+
+app.post('/create', (req, res) => {
+    res.send(req.body.name);
 });
 
 app.listen(8124);
