4039bb6659b89c79eb69be37e2b26d90e3a13acb
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-19 16:44:23 +0900

    Add codes

--- lesson16/index.js
+++ lesson16/index.js
@@ -1,11 +1,32 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const app = require('express')();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const initializePassport = require('./passport-config');
+
+initializePassport(
+    passport, email => users.find(user => user.email === email)
+);
+
 const users = [];
 
 app.set('views', __dirname + '/views');
 app.set('view-engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
@@ -15,9 +36,11 @@ app.get('/login', (req, res) => {
     res.render('login.ejs');
 });
 
-app.post('/login', (req, res) => {
-
-});
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
 
 app.get('/register', (req, res) => {
     res.render('register.ejs');
