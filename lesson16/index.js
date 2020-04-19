b052b8912d792cb26a96ec00b6022b835bb245ae
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-19 14:47:13 +0900

    Create files

--- lesson16/index.js
+++ lesson16/index.js
@@ -1,10 +1,29 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+const users = [];
 
 app.set('views', __dirname + '/views');
 app.set('view-engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs');
+});
+
+app.post('/login', (req, res) => {
+
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs');
+});
+
+app.post('/register', (req, res) => {
+
 });
 
 app.listen(8124);
