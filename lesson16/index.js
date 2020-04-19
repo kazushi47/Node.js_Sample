4f4d3c9593289001c5cc2dbb9bd9ffef052cc5ea
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-19 17:35:23 +0900

    Update index.js
    Repair code.

--- lesson16/index.js
+++ lesson16/index.js
@@ -8,6 +8,7 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
+const methodOverride = require('method-override');
 const initializePassport = require('./passport-config');
 
 initializePassport(
