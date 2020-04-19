9b83698c6d73367a42e66ca174561b398e86883d
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-19 16:51:46 +0900

    Update index.js
    Repair code.

--- lesson16/index.js
+++ lesson16/index.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 const app = require('express')();
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
