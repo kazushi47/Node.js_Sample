c210e53bd0d56dd5d171fe8afb0a2c3747cfea82
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-25 22:45:38 +0900

    Update files

--- myapp05/index.js
+++ myapp05/index.js
@@ -1,9 +1,17 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+const db = 'myapp05';
+const url = 'mariadb://localhost/' + db;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -17,12 +25,16 @@ app.get('/signin', (req, res) => {
     res.render('in');
 });
 
-app.post('/up', (req, res) => {
-    res.send('post ok');
+app.post('/signup', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    console.log('name: ' + name + ', email: ' + email);
 });
 
-app.post('/in', (req, res) => {
-    res.send('post ok');
+app.post('/signin', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    console.log('name: ' + name + ', email: ' + email);
 });
 
 http.listen(8124);
