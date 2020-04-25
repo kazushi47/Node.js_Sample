32cd3cca2cf0ca6996af9a9d49765e232f5147d9
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-25 23:11:39 +0900

    Update index.js

--- myapp05/index.js
+++ myapp05/index.js
@@ -28,7 +28,26 @@ app.get('/signin', (req, res) => {
 app.post('/signup', (req, res) => {
     const name = req.body.name;
     const email = req.body.email;
-    console.log('name: ' + name + ', email: ' + email);
+    if (name !== '' && email !== '') {
+        var flg = false;
+        MongoClient.connect(url, (err, client) => {
+            if (err) return console.dir(err);
+            client.db(db).collection('users', (err, collection) => {
+                const doc = {name: name, email: email};
+                collection.insertOne(doc, (err, result) => {
+                    console.dir(result);
+                    flg = true;
+                });
+            });
+        });
+        if (flg) {
+            res.send('success!!!');
+        } else {
+            res.send('エラーが発生しました。やり直してください。');
+        }
+    } else {
+        res.send('未入力項目があります。やり直してください。');
+    }
 });
 
 app.post('/signin', (req, res) => {
