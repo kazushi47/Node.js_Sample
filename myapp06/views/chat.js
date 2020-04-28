213db7cc869942db1b50a3e75a415d2f3d4b2867
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-28 23:59:04 +0900

    Update files

--- myapp06/views/chat.js
+++
@@ -1,8 +0,0 @@
-$(() => {
-    $('#form').submit((e) => {
-        e.preventDefault();
-        const message = $('#message').val();
-        $('#messages').append($('<li>').text(message));
-        $('#message').val('').focus();
-    });
-});
