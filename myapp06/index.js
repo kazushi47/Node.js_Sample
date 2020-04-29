9ac619efea68bc8958c8208d520e6241a291522e
kazushi47 <kazushisukaazu47@gmail.com>
2020-04-29 15:38:55 +0900

    Update index.js

--- myapp06/index.js
+++ myapp06/index.js
@@ -4,6 +4,12 @@ const http = require('http').createServer(app);
 const bodyParser = require('body-parser');
 const io = require('socket.io')(http);
 const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost';
+const dbName = 'myapp06';
+const connectOption = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -24,12 +30,27 @@ io.on('connection', (socket) => {
     socket.on('join', (data) => {
         socket.join('default-room');
         io.to('default-room').emit('join', data);
+        MongoClient.connect(url, connectOption, (err, client) => {
+            if (err) return console.dir(err);
+            const db = client.db(dbName);
+            const document = {name: data};
+            db.collection('users').insertOne(document, (err, res) => {
+                if (err) return console.dir(err);
+            });
+        });
     });
     socket.on('typing', (data) => {
         io.to('default-room').emit('typing', data);
     });
     socket.on('send', (data) => {
         io.to('default-room').emit('send', data);
+        MongoClient.connect(url, connectOption, (err, client) => {
+            if (err) return console.dir(err);
+            const db = client.db(dbName);
+            db.collection('messages').insertOne(data, (err, res) => {
+                if (err) return console.dir(err);
+            });
+        });
     });
 });
 
